df9a92eb571510ebdeab660bb3c1f901f31423a4
Jeferson <jefg89@gmail.com>
2021-08-24 09:33:51 +0200

    signed warning fix

--- common/scheduler/policies/migNextTile.cc
+++ common/scheduler/policies/migNextTile.cc
@@ -7,7 +7,7 @@ core_id_t MigNextTile::getMigrationCandidate(tile_id_t currentTile, const std::v
                                             const std::vector<UInt32> activeThreadsPerTile)
 {
 	core_id_t currentCore = -1;
-	for(tile_id_t tile_id = 0; tile_id < sharedTimePerTile.size(); tile_id++) {
+	for(tile_id_t tile_id = 0; tile_id < (tile_id_t)sharedTimePerTile.size(); tile_id++) {
 			if (tile_id != currentTile)
 				for (core_id_t core_id  = 0; core_id < 4; core_id++){
 					if (availableCores.at((tile_id*4  + core_id )))
