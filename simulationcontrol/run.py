c769ed4b328fd2673b7d10297e9031b1acd8fad8
Jeferson Gonzalez <jgonzalez@itcr.ac.cr>
2022-06-23 12:56:22 +0200

    changed run file to number of packets

--- simulationcontrol/run.py
+++ simulationcontrol/run.py
@@ -245,8 +245,9 @@ def test_static_power():
     run(['4.0GHz', 'testStaticPower', 'slowDVFS'], get_instance('parsec-blackscholes', 3, input_set='simsmall'))
 
 def test_custom_app(appname):
-    param = str(randint(128,255))
-    run(['4.0GHz', 'slowDVFS', 'maxFreq'], '{}-{}-{}'.format(appname, param, 1)) #+ ',{}-{}-{}'.format('myapps-rxtcc', 1,1) )
+    #param = str(randint(128,255))
+    packets = 8
+    run(['4.0GHz', 'slowDVFS', 'maxFreq'], '{}-{}-{}'.format(appname, packets, 1)) #+ ',{}-{}-{}'.format('myapps-rxtcc', 1,1) )
     #run(['4.0GHz', 'mediumDVFS', 'maxFreq'], '{}-{}-{}'.format('myapps-my_pi', 100000000,1) + ',{}-{}-{}'.format('myapps-my_pi', 100000000,1) + ',{}-{}-{}'.format('myapps-my_pi', 100000000,1) + ',{}-{}-{}'.format(appname,1000000, 1)  + ',{}-{}-{}'.format('myapps-my_pi', 100000000,1) + ',{}-{}-{}'.format('myapps-my_pi', 100000000,1) + ',{}-{}-{}'.format('myapps-my_pi', 100000000,1) + ',{}-{}-{}'.format('myapps-my_pi', 100000000,1) + ',{}-{}-{}'.format('myapps-my_pi', 1000000000,1) + ',{}-{}-{}'.format('myapps-my_pi', 1000000000,1) + ',{}-{}-{}'.format('myapps-my_pi', 1000000000,1) + ',{}-{}-{}'.format('myapps-my_pi', 1000000000,1) + ',{}-{}-{}'.format('myapps-my_pi', 100000000,1))
 
 
